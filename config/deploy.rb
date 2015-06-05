59d786ddd7f0ba2ab9be63dc172052a9a1d8c712
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2015-06-05 12:09:00 +0200

    making capistrano respect the given ref

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :deploy_to, '/home/heaven/heaven'
 
 # Default value for :scm is :git
 # set :scm, :git
+set :branch, (ENV['CAP_BRANCH'] || 'master')
 
 # Default value for :format is :pretty
 # set :format, :pretty
